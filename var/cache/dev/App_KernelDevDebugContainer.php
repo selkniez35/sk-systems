d450711da90ccdbd545c12dfd97180789157f1fc
selkniez <samy.elkniez@sksystems.fr>
2026-07-07 10:20:31 +0200

    Update environment configuration for production

--- var/cache/dev/App_KernelDevDebugContainer.php
+++ var/cache/dev/App_KernelDevDebugContainer.php
@@ -16,7 +16,7 @@ if (!\class_exists(App_KernelDevDebugContainer::class, false)) {
 
 return new \ContainerTOoZftL\App_KernelDevDebugContainer([
     'container.build_hash' => 'TOoZftL',
-    'container.build_id' => '6caf58aa',
-    'container.build_time' => 1783412259,
+    'container.build_id' => 'b349be1f',
+    'container.build_time' => 1783412401,
     'container.runtime_mode' => \in_array(\PHP_SAPI, ['cli', 'phpdbg', 'embed'], true) ? 'web=0' : 'web=1',
 ], __DIR__.\DIRECTORY_SEPARATOR.'ContainerTOoZftL');
