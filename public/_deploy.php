dcb7dd90910fc003054568aa2224801ebc964822
selkniez <samy.elkniez@sksystems.fr>
2026-07-08 23:51:19 +0200

    Add Symfony cache clearing step to deployment workflow and update asset paths

---
+++ public/_deploy.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Symfony\Bundle\FrameworkBundle\Console\Application;
+use Symfony\Component\Console\Input\ArrayInput;
+
+require dirname(__DIR__).'/vendor/autoload.php';
+
+$token = $_GET['token'] ?? '';
+
+if ($token !== $_ENV['DEPLOY_TOKEN']) {
+    http_response_code(403);
+    exit('Forbidden');
+}
+
+$kernel = new \App\Kernel('prod', false);
+
+$application = new Application($kernel);
+$application->setAutoExit(false);
+
+$application->run(new ArrayInput([
+    'command' => 'cache:clear',
+    '--env' => 'prod',
+    '--no-warmup' => true,
+]));
+
+$application->run(new ArrayInput([
+    'command' => 'cache:warmup',
+    '--env' => 'prod',
+]));
+
+echo 'OK';
--- public/build/app.dea52ec2.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[524],{28(s,e,o){o(736);console.log("SK Systems loaded"),console.log("This log comes from assets/app.js - welcome to AssetMapper! 🎉")}},s=>{s.O(0,[72],()=>{return e=28,s(s.s=e);var e});s.O()}]);
